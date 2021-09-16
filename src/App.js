4f9da3be29c1e6fea31c847ef8d67b1ef1f15a5a
aigerim0 <bolotovaaigerim42@gmail.com>
2021-09-16 05:36:41 +0300

    add  code

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import NotFount from "./views/NotFount";
 import Registration from "./views/Registration";
 import LogIn from "./views/LogIn";
 import Layout from "./Layout";
+import Claim from "./views/Claim";
 
 function App() {
     return (
@@ -16,6 +17,7 @@ function App() {
                <Route exact path='/news'> <News/> </Route>
                <Route path='/news/:id'> <NewsDetails/> </Route>
                <Route path='/login'><LogIn/></Route>
+               <Route path='/claim'><Claim/></Route>
                <Route path='/registration'><Registration/></Route>
                <Route path='*'><NotFount/></Route>
            </Switch>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './style.css'
 import { NavLink} from "react-router-dom";
+import axios from "axios";
 
 const Header = () => {
+    const [isOpen,setIsOpen] = useState(false)
+    const [user,setUser] = useState({name: "", phone:"", email:""})
+    const [sentSuccess,setSentSuccess] = useState(false)
+
+    const handleSubmit = (e) => {
+e.preventDefault()
+        axios.post('https://613b7037110e000017a45616.mockapi.io/api/callback',user)
+            .then(() => {
+                setSentSuccess(true)
+                setTimeout(() =>
+                    {
+                        setIsOpen(false)
+                        setSentSuccess(false)
+                    }
+                    ,2000)
+            } )
+    }
+
+    const handleChange = (e) => {
+        setUser({...user,[e.target.name]: e.target.value})
+    }
     return (
         <header className='header'>
 <div className='container'>
@@ -12,15 +34,46 @@ const Header = () => {
     </div>
 
     <div>
-        <ul className='nav-bar'>
-            <NavLink exact to={'/'} className='nav-bar-item'>< li>Главная</li></NavLink>
-            <NavLink to={'/news'} className='nav-bar-item'><li>Новости</li></NavLink>
-            <NavLink to={'/login'} className='nav-bar-item'><li>Войти</li></NavLink>
-            <NavLink to={'/registration'} className='nav-bar-item'><li>Зарегистироваться</li></NavLink>
-        </ul>
+        <div className='nav-bar'>
+            <NavLink exact to={'/'} className='nav-bar-item'>Главная</NavLink>
+            <NavLink to={'/news'} className='nav-bar-item'>Новости</NavLink>
+            <NavLink to={'/login'} className='nav-bar-item'>Войти</NavLink>
+            <NavLink to={'/claim'} className='nav-bar-item'>Заявки</NavLink>
+            <NavLink to={'/registration'} className='nav-bar-item'>Зарегистироваться</NavLink>
+            <button onClick={() => setIsOpen(true)} className='callback'>Связаться со мной</button>
+        </div>
+
     </div>
 </div>
 </div>
+            {
+                isOpen &&
+                <div className="modal-window">
+                    <div className='modal-content'>
+                        {
+                            sentSuccess ? 'Успешно отправлено!':
+                           <>
+                               <h3>Оставь заявку и получи консультацию от нас.</h3>
+                               <form onSubmit={handleSubmit}>
+                                   <div className='form-group'>
+                                       <label htmlFor='name' >Ваша имя:</label>
+                                       <input onChange={handleChange} className='form-input' type="text" id='name' name='name' required/>
+                                   </div>
+                                   <div className='form-group'>
+                                       <label htmlFor='phone' >Номер телефона:</label>
+                                       <input onChange={handleChange} className='form-input' type="text" id='phone' name='phone' required/>
+                                   </div>
+                                   <div className='form-group'>
+                                       <label htmlFor='email' >Почта:</label>
+                                       <input onChange={handleChange} className='form-input' type="text" id='email' name='email' required/>
+                                   </div>
+                                   <button className='callback sent-btn' type='submit'>Отправить</button>
+                               </form></>
+                        }
+                        <button onClick={() => setIsOpen(false)} className='close-modal'>×</button>
+                    </div>
+                </div>
+            }
         </header>
     );
 };
