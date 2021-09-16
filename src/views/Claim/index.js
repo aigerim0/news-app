c6c19478bbd64516833d04bc0f49f10af0b7a5ef
aigerim0 <bolotovaaigerim42@gmail.com>
2021-09-16 06:03:17 +0300

    add components

--- src/views/Claim/index.js
+++ src/views/Claim/index.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
 
 const Claim = () => {
+    const [claim,setClaim] = useState([])
+    useEffect(() => {
+        axios('https://613b7037110e000017a45616.mockapi.io/api/callback')
+            .then(({data}) =>setClaim(data) )
+    },[])
     return (
-        <div>
+        <div className='row'>
+            {
+                claim.map(item =>
+               <div className='col-md-4 ' key={item.id} >
+                  <div>
+                      <h5><b>Name:</b> {item.name}</h5>
+                  </div>
+                   <div>
+                       <p><b>Phone:</b> {item.phone}</p>
+                   </div>
+                   <div>
+                       <p><b>email:</b> {item.email}</p>
+                   </div>
+               </div>
 
+                )
+            }
         </div>
     );
 };
